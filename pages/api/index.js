e66b4482a641f75bbffb1b001f48465a9d9970d5
Marina Couto <marinacoutonina@gmail.com>
2024-02-08 16:36:44 -0300

    Atualizando index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,20 +1,17 @@
+const publicKey = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2a1dttjCODGWo9dIALqX
+6PAsZtJ7EZgX2AKxQEfFiSeb3H3gWk74CvexzmFWlgs5aGC+UmnoeMPaUk5i9O4S
+s4d8+qPKxlAOGBmfMeAiYBDAZ7aNfLwTQAAuGQ9e0woSL6gkvVnTCH8MbEaI1Erf
+7eQFRB9QDAhmtHK7oQrD0Q3e1GJV8SFNPs+1jdgZ4GLuHAGSFqKBjUTLdYhCjw3e
+iDsKkfUhZs51eBd512ijrZqyeCocF9Npmu9TzxXfpuegXtqKFy82DB6r8wMFEZ1U
+1ysP/cRgWCxcCjNaaTrw71FB9dNAu8BzoVHgN+MXJcxE4gveZzG53qXfdOwHTw5k
+OQIDAQAB
+-----END PUBLIC KEY-----`;
+
 export default function handler(req, res) {
-    const publicKey = `-----BEGIN PUBLIC KEY-----
-    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2a1dttjCODGWo9dIALqX
-    6PAsZtJ7EZgX2AKxQEfFiSeb3H3gWk74CvexzmFWlgs5aGC+UmnoeMPaUk5i9O4S
-    s4d8+qPKxlAOGBmfMeAiYBDAZ7aNfLwTQAAuGQ9e0woSL6gkvVnTCH8MbEaI1Erf
-    7eQFRB9QDAhmtHK7oQrD0Q3e1GJV8SFNPs+1jdgZ4GLuHAGSFqKBjUTLdYhCjw3e
-    iDsKkfUhZs51eBd512ijrZqyeCocF9Npmu9TzxXfpuegXtqKFy82DB6r8wMFEZ1U
-    1ysP/cRgWCxcCjNaaTrw71FB9dNAu8BzoVHgN+MXJcxE4gveZzG53qXfdOwHTw5k
-    OQIDAQAB
-    -----END PUBLIC KEY-----`;
-  
-    const dataCriacao = new Date('2024-02-07').getTime();
-  
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json({
-      public_key: publicKey,
-      created_at: dataCriacao
-    });
-  }
-  
+  // Envie apenas a chave pública no JSON de resposta
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json({
+    public_key: publicKey
+  });
+}
